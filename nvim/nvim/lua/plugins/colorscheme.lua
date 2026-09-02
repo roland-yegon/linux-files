bcf34323b8efaa790842a820b3fb1cf42219dee8
rolandyegon <rolandyegon@gmail.com>
2026-09-02 14:35:36 +0300

    Added new features to neovim

---
+++ nvim/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,         -- Enable transparent background
+      styles = {
+        sidebars = "transparent", -- Sidebars flat/transparent like nvim-tree
+        floats = "transparent",   -- Floating windows transparent
+      },
+    },
+  },
+}
