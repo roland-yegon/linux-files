18e73d1a7c330379c168fef20b4bb1c3ed8eb656
rolandyegon <rolandyegon@gmail.com>
2026-09-02 14:41:22 +0300

    Changed neovim to relative numbering

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.number = true -- Ensures absolute line number is shown on the current line
+vim.opt.relativenumber = false -- Disables relative line numbers
