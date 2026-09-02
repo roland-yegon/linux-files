927063edcee6156a14e26aadb76411dceab8e442
rolandyegon <rolandyegon@gmail.com>
2026-09-02 15:50:56 +0300

    Set nvim to use fish instead of bash

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Numbering
 vim.opt.number = true -- Ensures absolute line number is shown on the current line
 vim.opt.relativenumber = false -- Disables relative line numbers
+
+-- Shell
+vim.o.shell = "fish"
