60718d614daf340b69ba84489d03c5be4393450e
rolandyegon <rolandyegon@gmail.com>
2026-09-13 21:34:06 +0300

    Added softwrap to neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,10 +1,2 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Numbering
-vim.opt.number = true -- Ensures absolute line number is shown on the current line
-vim.opt.relativenumber = false -- Disables relative line numbers
-
--- Shell
-vim.o.shell = "bash"
+vim.opt.wrap = true
+vim.opt.linebreak = true -- Wrap lines at word boundaries instead of middle of a word
