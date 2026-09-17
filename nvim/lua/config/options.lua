d3225bb9f60fb08b8d3c89892b1153a5df3b3020
rolandyegon <rolandyegon@gmail.com>
2026-09-17 20:49:48 +0300

    Changed the nvim config files

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,2 +1,6 @@
-vim.opt.wrap = true
-vim.opt.linebreak = true -- Wrap lines at word boundaries instead of middle of a word
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.number = true
+vim.opt.relativenumber = false
